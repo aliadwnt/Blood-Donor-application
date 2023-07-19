30ddefe3193d2d42472977ca7021e9f3d1b6298a
Alia Dewanto <dewantoalia@gmail.com>
2023-07-19 12:20:30 +0700

    Add Files

---
+++ JavaGui/src/koneksilogin/basic.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksilogin;
+
+/**
+ *
+ * @author ASUS
+ */
+public class basic {
+    public static void main(String[] args){
+        home hm = new home();
+        hm.setVisible(true);
+        hm.pack();
+        hm.setLocationRelativeTo(null);
+        hm.setDefaultCloseOperation(home.EXIT_ON_CLOSE);
+    }
+    
+}
